a862b94875a05b5b0d289d575b108f89d0c4f0e0
Arnau González <arnaugm@gmail.com>
2019-06-22 13:01:36 +0200

    demo page served by aiohttp

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
+from settings import config, BASE_DIR
+
+
+@aiohttp_jinja2.template('hello.html')
+async def hello(request):
+    return {'greet': 'Hello', 'name': 'Arnau'}
+
+app = web.Application()
+app['config'] = config
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'app' / 'templates')))
+
+app.add_routes([web.get('/', hello)])
+app.router.add_static('/static/',
+                      path=BASE_DIR / 'static',
+                      name='static')
+web.run_app(app)
