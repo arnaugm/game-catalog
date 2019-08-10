02cc892f18c6565b492ba47eaa2d830724b05ccf
Arnau González <arnaugm@gmail.com>
2019-08-11 01:53:53 +0200

    remove use of jinja, redirect root path to client build folder

--- app/main.py
+++ app/main.py
@@ -1,24 +1,19 @@
 from aiohttp import web
-import aiohttp_jinja2
-import jinja2
 import os
 
 from settings import config, BASE_DIR
 
 
-@aiohttp_jinja2.template('hello.html')
-async def hello(request):
-    return {'greet': 'Hello', 'name': 'Arnau'}
+async def index(request):
+    return web.FileResponse(BASE_DIR / 'public' / 'index.html')
 
 app = web.Application()
 app['config'] = config
-aiohttp_jinja2.setup(app,
-    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'app' / 'templates')))
 
-# app.add_routes([web.get('/', hello)])
-app.router.add_static('/',
-                      path=BASE_DIR / 'public/static',
-                      name='public')
+app.add_routes([web.get('/', index)])
+app.router.add_static('/static/',
+                      path=BASE_DIR / 'public' / 'static',
+                      name='static')
 
 port = int(os.environ.get("PORT", 8080))
 web.run_app(app, port=port)
