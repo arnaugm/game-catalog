9ef81f78adddd68e98c5c7567ee96b051935d65b
Arnau González <arnaugm@gmail.com>
2019-08-18 00:16:44 +0200

    add simple test

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ import pathlib
 import yaml
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
-config_path = BASE_DIR / 'config' / 'config.yaml'
+config_path = BASE_DIR / "config" / "config.yaml"
 
 
 def get_config(path):
